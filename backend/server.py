2ae181e84ddbc4457d144421f3fd78967215aba0
soulfly8713 <moderatorsolu@gmail.com>
2025-11-23 20:29:29 +0300

    Test

--- backend/server.py
+++ backend/server.py
@@ -195,7 +195,12 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=[
+        "https://yenisitem.vercel.app", 
+        "https://www.yenisitem.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
